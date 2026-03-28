ba154278ba3c13753a5bbc184f3c7e6674145606
Travis Brown <travislbrown@gmail.com>
2026-03-28 13:19:58 -0500

    chore: remove temp files

--- do-commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\Users\TBrown\Desktop\rx-skin
-git add -A
-git commit -m "chore: remove stale batch files and build artifacts"
-git push origin main
-git log --oneline -3
-del do-commit.bat
